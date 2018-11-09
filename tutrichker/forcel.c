807bef12418a6d79321a7aebd4e716d78784c262
roboscales <44896132+roboscales@users.noreply.github.com>
2018-11-10 01:34:31 +0300

    Create forcel.c

---
+++ tutrichker/forcel.c
@@ -0,0 +1,23 @@
+//this is very very hard
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+float fahr, celsius;
+int lower, upper, step;
+
+lower = 0;
+upper = 300;
+step = 20;
+
+fahr = lower;
+
+while(fahr <= upper)
+{
+    celsius = (5.0/9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+}
+return 0;
+}
