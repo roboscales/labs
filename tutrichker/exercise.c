b51ebf5f631c55dfac1d296be4e14136450601f1
roboscales <44896132+roboscales@users.noreply.github.com>
2018-11-09 22:40:11 +0300

    Add files via upload

---
+++ tutrichker/exercise.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    float fahr, cel;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 500;
+    ster = 20;
+
+    cel = lower;
+
+    while (cel <= upper)
+    {
+        fahr = 32 + c * (5.0/9.0);
+        printf("%-4.0f %6.1f",celsius, fahr);
+        cel += step;
+    }
+    return 0;
+}
